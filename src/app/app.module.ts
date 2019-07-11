80766f65a9608f83888f5249bfc4e990caf49e6c
Ludovico Brocca <lbrocca29@gmail.com>
2019-07-11 10:36:37 +0200

    some minor fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { CoreModule } from './core/core.module';
 import { CustomersModule } from './customers/customer.module';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { OrdersModule } from './orders/orders.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    CoreModule,
-    CustomersModule,
-    SharedModule,
-    AppRoutingModule,
-    OrdersModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  exports: [CustomersModule]
+  imports:      [ BrowserModule, CustomersModule, SharedModule, CoreModule, AppRoutingModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
